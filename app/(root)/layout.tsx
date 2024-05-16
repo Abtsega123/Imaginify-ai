31b457459e13b177d25eb2dd34ea7369a92f9df3
Abtsega123 <andyboringo1@gmail.com>
2024-05-17 02:08:18 +0530

    commited

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,25 @@
+import Sidebar from '@/components/shared/Sidebar';
+import MobileNav from '@/components/shared/MobileNav';
+import React, { Children } from 'react'
+import { Toaster } from '@/components/ui/toaster';
+
+const Layout = ({ children}: {children: React.ReactNode}) =>{
+    return (
+        <main className='root'>
+
+            <Sidebar />
+            <MobileNav /> 
+            <div className='root-container'>
+                <div className='wrapper'>
+                    {children}
+                </div>
+            </div>
+            <Toaster />
+
+
+            </main>
+    )
+}
+
+export default Layout;
+
---
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -0,0 +1,60 @@
+import Header from '@/components/shared/Header'
+import TransformationForm from '@/components/shared/TransformationForm';
+
+
+import { transformationTypes } from '@/constants'
+import { getUserById } from '@/lib/actions/user.actions';
+import { auth } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
+
+
+const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps) => {
+
+  const {userId} = auth()
+  const transformation = transformationTypes[type];
+ 
+
+  if (!userId) {
+    // Redirect to the sign-in page if userId is not available
+    redirect('/sign-in');
+    return null; // Return early to avoid further execution
+  }
+
+  try {
+    // Assuming getUserById fetches user data from somewhere
+    // const user = await getUserById(userId);
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    // Handle the error accordingly
+  }
+
+ 
+  
+
+  return (
+   <>
+      <Header 
+        title={transformation.title}
+        subtitle={transformation.subTitle}
+      />
+      <section className='mt-10'>
+      <TransformationForm
+    action= "Add"
+    userId={userId._id}
+    type={transformation.type as TransformationTypeKey}
+    creditBalance={userId.creditBalance}
+   
+  
+  />
+      </section>
+      
+    
+      </>
+      
+      
+      
+    
+  )
+}
+
+export default AddTransformationTypePage
